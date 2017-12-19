4088a82a23db42d02a9e231435e473d691332c13
baayso <baayso@outlook.com>
2017-12-19 14:30:44 +0800

    first commit.

---
+++ app/service/some.js
@@ -0,0 +1,13 @@
+// app/service/some.js
+
+const Service = require('egg').Service;
+
+class SomeService extends Service {
+
+    async list() {
+        const rule = this.config.robot.ua;
+    }
+
+}
+
+module.exports = SomeService;
---
+++ config/config.default.js
@@ -0,0 +1,38 @@
+// config/config.default.js
+
+module.exports = appInfo => {
+    const config = exports = {};
+
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1512550668555_3966';
+
+    // add your config here
+    config.middleware = [];
+
+    config.view = {
+        defaultViewEngine: 'nunjucks',
+        mapping: {
+            '.tpl': 'nunjucks',
+        },
+    };
+
+    config.news = {
+        pageSize: 5,
+        serverUrl: 'https://hacker-news.firebaseio.com/v0',
+    };
+
+    // add middleware robot
+    config.middleware = [
+        'robot',
+    ];
+
+    // robot's configurations
+    config.robot = {
+        ua: [
+            /curl/i,
+            /Baiduspider/i,
+        ],
+    };
+
+    return config;
+};
